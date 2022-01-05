63795e9b407a636bd43dbb2a2b16eebeb98cf6ee
Felix Weilbach <felix.weilbach@t-online.de>
2022-01-05 14:11:17 +0100

    Show window

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,228 @@
-int main() {}
+// clang-format off
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+// clang-format on
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+namespace
+{
+constexpr auto opengl_version_major = 4;
+constexpr auto opengl_version_minor = 6;
+
+constexpr auto opengl_debug = true;
+
+constexpr auto window_width  = 1280;
+constexpr auto window_height = 720;
+
+constexpr auto window_title = "OpenGL App";
+
+void glfw_error_callback(int error_code, const char *description)
+{
+  std::cerr << "GLFW Error [" << error_code << "]: " << description
+            << std::endl;
+}
+
+void APIENTRY gl_debug_callback(GLenum source,
+                                GLenum type,
+                                GLuint id,
+                                GLenum severity,
+                                GLsizei /*length*/,
+                                const GLchar *msg,
+                                const void * /*param*/)
+{
+  std::string source_str;
+  switch (source)
+  {
+  case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+    source_str = "WindowSys";
+    break;
+  case GL_DEBUG_SOURCE_APPLICATION:
+    source_str = "App";
+    break;
+  case GL_DEBUG_SOURCE_API:
+    source_str = "OpenGL";
+    break;
+  case GL_DEBUG_SOURCE_SHADER_COMPILER:
+    source_str = "ShaderCompiler";
+    break;
+  case GL_DEBUG_SOURCE_THIRD_PARTY:
+    source_str = "3rdParty";
+    break;
+  case GL_DEBUG_SOURCE_OTHER:
+    source_str = "Other";
+    break;
+  default:
+    source_str = "Unknown";
+  }
+
+  std::string type_str;
+  switch (type)
+  {
+  case GL_DEBUG_TYPE_ERROR:
+    type_str = "Error";
+    break;
+  case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+    type_str = "Deprecated";
+    break;
+  case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+    type_str = "Undefined";
+    break;
+  case GL_DEBUG_TYPE_PORTABILITY:
+    type_str = "Portability";
+    break;
+  case GL_DEBUG_TYPE_PERFORMANCE:
+    type_str = "Performance";
+    break;
+  case GL_DEBUG_TYPE_MARKER:
+    type_str = "Marker";
+    break;
+  case GL_DEBUG_TYPE_PUSH_GROUP:
+    type_str = "PushGrp";
+    break;
+  case GL_DEBUG_TYPE_POP_GROUP:
+    type_str = "PopGrp";
+    break;
+  case GL_DEBUG_TYPE_OTHER:
+    type_str = "Other";
+    break;
+  default:
+    type_str = "Unknown";
+  }
+
+  std::string sev_str;
+  switch (severity)
+  {
+  case GL_DEBUG_SEVERITY_HIGH:
+    sev_str = "HIGH";
+    break;
+  case GL_DEBUG_SEVERITY_MEDIUM:
+    sev_str = "MED";
+    break;
+  case GL_DEBUG_SEVERITY_LOW:
+    sev_str = "LOW";
+    break;
+  case GL_DEBUG_SEVERITY_NOTIFICATION:
+    sev_str = "NOTIFY";
+    break;
+  default:
+    sev_str = "UNK";
+  }
+
+  std::cerr << "OpenGL" << source_str << ":" << type_str << "[" << sev_str
+            << "]"
+            << "(" << id << "): " << msg << std::endl;
+}
+
+void gl_dump_info()
+{
+  const GLubyte *renderer     = glGetString(GL_RENDERER);
+  const GLubyte *vendor       = glGetString(GL_VENDOR);
+  const GLubyte *version      = glGetString(GL_VERSION);
+  const GLubyte *glsl_version = glGetString(GL_SHADING_LANGUAGE_VERSION);
+
+  GLint major, minor, samples, sampleBuffers;
+  glGetIntegerv(GL_MAJOR_VERSION, &major);
+  glGetIntegerv(GL_MINOR_VERSION, &minor);
+  glGetIntegerv(GL_SAMPLES, &samples);
+  glGetIntegerv(GL_SAMPLE_BUFFERS, &sampleBuffers);
+
+  std::printf(
+      "-------------------------------------------------------------\n");
+  std::printf("GL Vendor    : %s\n", vendor);
+  std::printf("GL Renderer  : %s\n", renderer);
+  std::printf("GL Version   : %s\n", version);
+  std::printf("GL Version   : %d.%d\n", major, minor);
+  std::printf("GLSL Version : %s\n", glsl_version);
+  std::printf("MSAA samples : %d\n", samples);
+  std::printf("MSAA buffers : %d\n", sampleBuffers);
+  std::printf(
+      "-------------------------------------------------------------\n");
+}
+} // namespace
+
+int main()
+{
+  if (!glfwInit())
+  {
+    return EXIT_FAILURE;
+  }
+
+  glfwSetErrorCallback(glfw_error_callback);
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, opengl_version_major);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, opengl_version_minor);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+  if (opengl_debug)
+  {
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+  }
+
+  const auto window = glfwCreateWindow(window_width,
+                                       window_height,
+                                       window_title,
+                                       nullptr,
+                                       nullptr);
+  if (!window)
+  {
+    std::cerr << "Could not create GLFW window" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // Set here instead of nullptr data you want to receive in the callback
+  glfwSetWindowUserPointer(window, nullptr);
+  glfwSetMonitorUserPointer(glfwGetPrimaryMonitor(), nullptr);
+
+  glfwMakeContextCurrent(window);
+
+  // Enable vsync
+  glfwSwapInterval(0);
+
+  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+  if (!gladLoadGL())
+  {
+    std::cerr << "GLAD could not load OpenGL" << std::endl;
+  }
+
+  if (opengl_debug)
+  {
+    glDebugMessageCallback(gl_debug_callback, nullptr);
+    glDebugMessageControl(GL_DONT_CARE,
+                          GL_DONT_CARE,
+                          GL_DONT_CARE,
+                          0,
+                          nullptr,
+                          GL_TRUE);
+    glDebugMessageInsert(GL_DEBUG_SOURCE_APPLICATION,
+                         GL_DEBUG_TYPE_MARKER,
+                         0,
+                         GL_DEBUG_SEVERITY_NOTIFICATION,
+                         -1,
+                         "Start debugging");
+  }
+
+  gl_dump_info();
+
+  glEnable(GL_DEPTH_TEST);
+
+  while (true)
+  {
+    glfwPollEvents();
+
+    glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+    glClearColor(0.4f, 0.4f, 0.4f, 1.0f);
+
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+      break;
+    }
+
+    glfwSwapBuffers(window);
+  }
+}
